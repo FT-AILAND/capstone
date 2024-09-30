58ac6179322fb1638ed18adb69622e0a88d3d89c
joonheui <iuehnoojmik@naver.com>
2024-09-30 23:34:05 +0900

    Update : Feedback Page, Help page

--- lib/Pages/goal.dart
+++ lib/Pages/goal.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class goalPage extends StatefulWidget {
@@ -8,8 +9,201 @@ class goalPage extends StatefulWidget {
 }
 
 class _goalPageState extends State<goalPage> {
+  // List to store repetitions for each exercise
+  List<int> _repetitions = [0, 0, 0]; // Initial repetitions for each exercise
+  bool _isRepetitionSelected = false;
+
+  // Function to show repetition picker for a specific exercise
+  void _showRepetitionPicker(int idx) {
+    int tempRepetition = _repetitions[idx]; // Temporary value for repetition
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) => Container(
+        height: 300,
+        padding: const EdgeInsets.only(top: 6.0),
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        child: SafeArea(
+          top: false,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CupertinoButton(
+                    child: const Text(
+                      '취소',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the picker
+                    },
+                  ),
+                  CupertinoButton(
+                    child: const Text(
+                      '선택',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _repetitions[idx] =
+                            tempRepetition; // Set the selected repetition for this exercise
+                        _isRepetitionSelected = true;
+                      });
+                      Navigator.of(context).pop(); // Close the picker
+                    },
+                  ),
+                ],
+              ),
+              Expanded(
+                child: CupertinoPicker(
+                  magnification: 1.22,
+                  squeeze: 1.2,
+                  useMagnifier: true,
+                  itemExtent: 32,
+                  scrollController: FixedExtentScrollController(
+                    initialItem:
+                        _repetitions[idx] - 1, // Initialize to current value
+                  ),
+                  onSelectedItemChanged: (int selectedItem) {
+                    tempRepetition = selectedItem + 1; // Update temp value
+                  },
+                  children: List<Widget>.generate(100, (int index) {
+                    return Center(
+                      child: Text(
+                        '${index + 1}',
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF3D3F5A),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF3D3F5A),
+          title: const Center(
+            child: Text(
+              "목표",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 30,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Text(
+                          '일일 목표 개수 ',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF595B77).withOpacity(0.5),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0x595B77),
+                      spreadRadius: 2,
+                      blurRadius: 7.0,
+                      offset: Offset(2, 5), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: SizedBox(
+                  width: 400,
+                  height: 110,
+                  child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (BuildContext ctx, int idx) {
+                      List<String> exercises = ['Push up', 'Pull up', 'Squat'];
+                      return InkWell(
+                        onTap: () {
+                          // Show the picker when the item is tapped
+                          _showRepetitionPicker(idx);
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                  child: Text('${exercises[idx]}',
+                                      style: TextStyle(color: Colors.white))),
+                              Row(
+                                children: [
+                                  Text(
+                                    '${_repetitions[idx]}', // Display the selected repetition
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(
+                                      width:
+                                          10), // Space between number and icon
+                                  const Icon(
+                                    Icons.settings,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Pages/workout_result_page.dart
+++ lib/Pages/workout_result_page.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '/utils/function_utils.dart';
+import 'package:ait_project/main.dart';
 import '/Navigator/customized_bar_chart.dart';
 import '/models/workout_result.dart';
-import '/utils/function_utils.dart';
 
 // ignore: must_be_immutable
 class WorkoutResultPage extends StatelessWidget {
   WorkoutResultPage({Key? key, required this.workoutResult}) : super(key: key);
   WorkoutResult workoutResult;
 
+  String getKoreanWorkoutName(String? workoutName) {
+    switch (workoutName) {
+      case 'squat':
+        return '스쿼트';
+      case 'push_up':
+        return '푸시업';
+      case 'pull_up':
+        return '풀업';
+      default:
+        return workoutName ?? '';
+    }
+  }
+
+  String getFormattedTimestamp(DateTime? timestamp) {
+    if (timestamp == null) return '날짜 정보 없음';
+    return DateFormat('yyyy-MM-dd HH:mm').format(timestamp);
+  }
+
+  String _getWorkoutImage(String? workoutName) {
+    switch (workoutName) {
+      case 'squat':
+        return 'assets/squat.gif';
+      case 'push_up':
+        return 'assets/pushUp.gif';
+      case 'pull_up':
+        return 'assets/pullUp.gif';
+      default:
+        return 'assets/test.gif'; // 기본 이미지 경로
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text(
+          '분석 결과',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        centerTitle: true,
         backgroundColor: const Color(0xFF3D3F5A),
         elevation: 0,
         leading: IconButton(
@@ -19,35 +62,121 @@ class WorkoutResultPage extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Container(
-                height: 300,
-                width: 300,
-                child: CustomizedBarChart(
-                  workoutResult: workoutResult,
+
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                _getWorkoutImage(workoutResult.workoutName),
+                                width: 40,
+                                height: 40,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  getKoreanWorkoutName(workoutResult.workoutName),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w900),
+                                ),
+                                Text(
+                                  getFormattedTimestamp(workoutResult.timestamp),
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      '${workoutResult.count}회',
+                      style: const TextStyle(
+                        color: Color(0XFF9FA2CE),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w900),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            ClipRRect(
+        
+              // 피드백 그래프
+              ClipRRect(
                 borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  color: Color(0xffFFE6D6),
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: _buildFeedback(),
-                    ),
+                child: Center(
+                  child: CustomizedBarChart(
+                    workoutResult: workoutResult,
                   ),
                 ),
               ),
-          ],
+        
+              // 피드백 텍스트
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: aitNavy,
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15, right: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              const Text(
+                                '운동 피드백',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              const SizedBox(width: 10),  // 텍스트와 구분선 사이의 간격
+                              Expanded(
+                                child: Container(
+                                  height: 1,  // 구분선의 높이
+                                  color: Colors.white,  // 구분선의 색상
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Center(
+                            child: _buildFeedback(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
@@ -64,41 +193,51 @@ class WorkoutResultPage extends StatelessWidget {
       // squat
       feedbackString = SquatFeedbackString;
     }
-    String feedbackResult = "";
-    int num = 2;
-    for (int i in feedbackIdx) {
-      if (num == 0) break;
-      feedbackResult += feedbackString[i] + '\n';
-      num--;
-    }
-    return Text(
-      feedbackResult,
+    
+    return Column(
+      children: [
+        const SizedBox(height: 10),  // 상단 여백 추가
+        ...feedbackIdx.take(2).map((i) => 
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),  // 각 피드백 항목의 상하 패딩
+            child: Text(
+              feedbackString[i],
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+        ).toList(),
+        const SizedBox(height: 10),  // 하단 여백 추가
+      ],
     );
   }
 }
 
 List<String> SquatFeedbackString = [
-  '''이완을 더 해주세요. 이완을 통해 근섬유의 길이가 더 길어지며, 비대해질 수 있습니다.''',
-  '''수축을 더 해주세요. 수축을 제대로 하지 않으면 운동효과를 기대하기 어렵습니다.''',
-  '''엉덩이가 무릎보다 먼저 수축하고 있습니다. 무릎과 엉덩이가 동시에 수축해야 근육이 균등하게 발달할수 있습니다.''',
-  '''무릎이 엉덩이보다 먼저 수축하고 있습니다. 무릎과 엉덩이가 동시에 수축해야 근육이 균등하게 발달할수 있습니다.''',
-  '''무릎이 발끝보다 앞으로 나가고 있습니다. 무릎이 많이 나오면 무릎관절에 무리가 갈 수 있습니다.''',
-  '''너무 빠른속도로 운동하고 있습니다. 자세에 신경쓰고 근육의 이완과 수축을 느끼며 운동해보세요.'''
+  '''신장 단계를 더 강조해 주세요. 적절한 근육 신장은 근섬유의 길이를 증가시켜 근비대와 가동 범위 향상에 도움이 됩니다.''',
+  '''수축 강도를 높여주세요. 충분한 근수축이 이루어지지 않으면 근력 향상과 근비대 효과가 제한될 수 있습니다.''',
+  '''고관절(엉덩이)이 슬관절(무릎)보다 먼저 신전되고 있습니다. 대퇴사두근과 둔근의 균형 잡힌 발달을 위해 동시에 신전되어야 합니다.''',
+  '''슬관절(무릎)이 고관절(엉덩이)보다 먼저 신전되고 있습니다. 대퇴사두근과 둔근의 균형 잡힌 발달을 위해 동시에 신전되어야 합니다.''',
+  '''슬관절(무릎)이 발끝을 넘어가고 있습니다. 이는 슬개건에 과도한 스트레스를 줄 수 있으며, 장기적으로 슬관절 통증을 유발할 수 있습니다.''',
+  '''동작 속도가 과도하게 빠릅니다. 근육의 신장-수축 주기를 충분히 느끼며, 올바른 자세로 천천히 수행하는 것이 근육 활성화에 더 효과적입니다.'''
 ];
 
 List<String> PullUpFeedbackString = [
-  '''이완을 더 해주세요. 이완을 통해 근섬유의 길이가 더 길어지며, 비대해질 수 있습니다.''',
-  '''수축을 더 해주세요. 수축을 제대로 하지 않으면 운동효과를 기대하기 어렵습니다.''',
-  '''운동을 하면서 팔이 흔들리고 있습니다. 전완근을 사용해서 운동하는 것이 아닌 등에 초점을 맞춰주세요.''',
-  '''반동을 사용해 운동하고 있습니다. 운동효과가 떨어질 수 있어요. 만약 힘에 부친다면 밴드를 발에 걸어 운동해보세요.''',
-  '''너무 빠른속도로 운동하고 있습니다. 자세에 신경쓰고 근육의 이완과 수축을 느끼며 운동해보세요.'''
+  '''신장 단계를 더 강조해 주세요. 충분한 광배근과 상완이두근의 신장은 근섬유 길이 증가와 근력 향상에 중요합니다.''',
+  '''수축 강도를 높여주세요. 특히 광배근의 완전한 수축은 상체 근력과 근비대에 필수적입니다.''',
+  '''팔의 흔들림이 관찰됩니다. 광배근과 대원근에 집중하여 등 근육을 주도적으로 사용하세요. 이는 더 효과적인 상체 근력 발달을 돕습니다.''',
+  '''과도한 반동을 사용하고 있습니다. 이는 근육의 집중적 활성화를 감소시킵니다. 저항밴드를 활용하여 보조받는 풀업으로 시작해 보세요.''',
+  '''동작 속도가 과도하게 빠릅니다. 근육의 신장-수축 주기를 충분히 느끼며, 올바른 자세로 천천히 수행하는 것이 근육 활성화와 근력 향상에 더 효과적입니다.'''
 ];
 
 List<String> PushUpFeedbackString = [
-  '''이완을 더 해주세요. 이완을 통해 근섬유의 길이가 더 길어지며, 비대해질 수 있습니다.''',
-  '''수축을 더 해주세요. 수축을 제대로 하지 않으면 운동효과를 기대하기 어렵습니다.''',
-  '''골반이 올라간 상태로 운동하고 있습니다. 어깨, 팔꿈치 손목에 부상을 당할 수 있는 자세이면서 운동효과가 떨어질 수 있어요.''',
-  '''골반이 내려간 상태로 운동하고 있습니다. 하중이 내려간 자세라서 횟수를 쉽게 늘릴 수 있지만, 올바른 운동효과를 기대하기 어려워요.''',
-  '''무릎이 접힌 상태로 운동하고 있습니다. 하중이 내려간 자세라서 횟수를 쉽게 늘릴 수 있지만, 올바른 운동효과를 기대하기 어려워요.''',
-  '''너무 빠른속도로 운동하고 있습니다. 자세에 신경쓰고 근육의 이완과 수축을 느끼며 운동해보세요.'''
+  '''신장 단계를 더 강조해 주세요. 대흉근과 상완삼두근의 충분한 신장은 근섬유 길이 증가와 근력 향상에 중요합니다.''',
+  '''수축 강도를 높여주세요. 특히 대흉근과 상완삼두근의 완전한 수축은 상체 근력과 근비대에 필수적입니다.''',
+  '''골반이 과도하게 상승된 상태입니다. 이는 견갑골, 주관절, 수근관절에 부상 위험을 증가시키며, 대흉근과 삼각근의 효과적인 활성화를 저해합니다.''',
+  '''골반이 과도하게 하강된 상태입니다. 이는 복근의 적절한 긴장을 방해하고, 요추에 과도한 스트레스를 줄 수 있습니다. 코어를 단단히 유지하세요.''',
+  '''슬관절(무릎)이 굴곡된 상태입니다. 이는 전신의 근육 연쇄를 방해하여 코어와 대퇴사두근의 적절한 활성화를 저해합니다. 전신을 일직선으로 유지하세요.''',
+  '''동작 속도가 과도하게 빠릅니다. 근육의 신장-수축 주기를 충분히 느끼며, 올바른 자세로 천천히 수행하는 것이 근육 활성화와 근력 향상에 더 효과적입니다.'''
 ];
